5217b68589e9ce35b981a9f1f97dd1034bc3a41a
alsta017 <alsta017@gmail.com>
2023-09-21 09:42:34 +0200

    namechange and index update
    namechange
    index update to autocomplete
    small fixes to make it work
    more ig

--- velgstasjon.js
+++ velgstasjon.js
@@ -1,7 +1,13 @@
+var inputStasjonEl = document.getElementById("inputStasjon")
 var velgenEl = document.getElementById("velgen")
 var velgEnArr = [];
 var a = 0;
-function velgenstasjon() {
+
+
+inputStasjonEl.onkeydown = function () {
+    if (inputStasjonEl.childNodes.length = 0) {
+        alert("Skriv inn noe")
+    } else {
     var stasjonInputel = document.getElementById("inputStasjon");
     fetch(`https://api.entur.io/geocoder/v1/autocomplete?text=${stasjonInputel.value}`, {
             headers: {
@@ -10,6 +16,9 @@ function velgenstasjon() {
         })
         .then(response => response.json())
         .then(data => {
+            velgEnArr = [];
+            velgenEl.textContent = "";
+            a = 0;
             for (x = 0; x < data.features.length; x++) {
                 if (data.features[x].properties.id.includes("NSR:StopPlace")) {
                     a++
@@ -18,13 +27,16 @@ function velgenstasjon() {
                     stasjonspelement.className = "stasjonspelement";
                     stasjonsbutton.className = "stasjonsbutton";
                     stasjonsbutton.setAttribute("id", `${a}`)
+                    stasjonspelement.setAttribute("id", `${a}`)
                     stasjonsbutton.setAttribute("onclick", "buttonclicked(this.id)")
+                    stasjonspelement.setAttribute("onclick", "buttonclicked(this.id)")
                     stasjonspelement.textContent = data.features[x].properties.name + ", " + data.features[x].properties.locality;
                     var velgEnArrel = data.features[x].properties.id;
-                    stasjonsbutton.textContent = "Velg"
+                    stasjonsbutton.textContent = "Velg";
                     stasjonspelement.appendChild(stasjonsbutton);
                     velgenEl.appendChild(stasjonspelement)
                     velgEnArr.push(velgEnArrel);
+                    console.log(velgEnArr);
                     // localStorage.setItem("ID", stasjonsID);
                     // window.location.replace("avganger.html")
                 }
@@ -33,7 +45,9 @@ function velgenstasjon() {
         .catch(error => {
             console.error("Error fetching data:", error);
             console.log("Error fetching data.");
+            alert("Error fetching data.");
         });
+    }
     }
 function buttonclicked(clicked_id) {
     localStorage.setItem("ID", velgEnArr[clicked_id - 1])
